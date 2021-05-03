959a2205e729e59968c8ac55738578b364e2b562
DaniyalShaikh-0 <daniyalshaikh70@gmail.com>
2021-05-03 23:07:48 +0500

    Added Input along with props in Button Component

--- app/Components/ButtonsComponent.js
+++ app/Components/ButtonsComponent.js
@@ -11,33 +11,57 @@ import {
   TouchableOpacity,
   ScrollView,
   FlatList,
+  ImageBackground,
 } from "react-native";
+import CurrGoal from "./TextInputComponent";
 const Buttons = (props) => {
-  console.log("PRO, : ", props);
+  // console.log("PRO, : ", props);
   return (
     <Modal visible={props.mode} animationType="fade">
-      <View style={props.viewStyle}>
-        <Pressable
-          onPress={() => props.modeManager(false)}
-          style={({ pressed }) => props.CloseButton}
-        >
-          <Text style={props.CloseButton}>Close</Text>
-        </Pressable>
-        <Button title="ADD" color="#66ff33" onPress={props.add}></Button>
-        <View style={{ padding: 5 }}></View>
-        <View></View>
-        <Button
-          title="DELETE current"
-          color="orange"
-          onPress={props.delCur} //deleteCurrent
-        ></Button>
-        <View style={{ padding: 5 }}></View>
-        <Button
-          title="DELETE old tasks"
-          color="red"
-          onPress={props.delPrev} //deletePrev
-        ></Button>
-      </View>
+      <ImageBackground
+        source={require("../assets/Scenery.jpg")}
+        style={{
+          flex: 1,
+          // alignContent: "flex-end",
+          width: "100%",
+          height: "100%",
+          // marginTop: "10%",
+          // justifyContent: "center",
+        }}
+      >
+        <View style={props.viewStyle}>
+          <Pressable
+            onPress={() => props.modeManager(false)}
+            style={({ pressed }) => props.CloseButton}
+          >
+            <Text style={props.CloseButton}>Close</Text>
+          </Pressable>
+          <Button title="ADD" color="#66ff33" onPress={props.add}></Button>
+          <View style={{ padding: 5 }}></View>
+          <View></View>
+          <Button
+            title="DELETE current"
+            color="orange"
+            onPress={props.delCur} //deleteCurrent
+          ></Button>
+          <View style={{ padding: 5 }}></View>
+          <Button
+            title="DELETE old tasks"
+            color="red"
+            onPress={props.delPrev} //deletePrev
+          ></Button>
+        </View>
+
+        <View style={props.layer} />
+        <View style={props.inputStyle}>
+          <CurrGoal handleGoal={props.goalManager} value={props.inpGoal} />
+          {/* <TextInput style={styles.inputStyle}
+      placeholder='Goals'
+      placeholderTex tColor='#000000'
+      onChangeText={goalManager}>
+    </TextInput> */}
+        </View>
+      </ImageBackground>
     </Modal>
   );
 };
